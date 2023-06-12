b4f994e3d4f543f05a162ae776b1435a24964409
ChristopherJ123 <christopherz2002@hotmail.com>
2023-06-12 17:33:47 +0700

    tweak

--- minecraft/update_users_online_status_id.php
+++ minecraft/update_users_online_status_id.php
@@ -1,11 +1,16 @@
 <?php
 include('database.php');
 
-$query = "
-UPDATE users_online
-SET status_id = 0
-WHERE end_time < CURRENT_TIMESTAMP
-";
-mysqli_query($conn, $query);
+try {
+    $query = "
+    UPDATE users_online
+    SET status_id = 0
+    WHERE end_time < CURRENT_TIMESTAMP
+    ";
+    mysqli_query($conn, $query);
+    echo "Success!";
+} catch (mysqli_sql_exception) {
+    echo "System error!";
+}
 
 mysqli_close($conn);
