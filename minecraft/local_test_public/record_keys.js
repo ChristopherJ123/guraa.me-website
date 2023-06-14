d687e56a6d1e235fec8be864d4e2248580b3f29e
ChristopherJ123 <christopherz2002@hotmail.com>
2023-06-14 13:15:27 +0700

    test2

--- minecraft/local_test_public/record_keys.js
+++ minecraft/local_test_public/record_keys.js
@@ -5,8 +5,24 @@ document.addEventListener('keypress', (e) => {
     document.querySelector('.key').innerText += key;
 })
 
-function keyPressed(key) {
-    document.querySelector('.key').innerText += key;
+function keyPressed(value) {
+    document.querySelector('.key').innerText += value;
+
+    if (value.length == 0) {
+        document.getElementById('search-output').innerHTML = "";
+        document.getElementById('search-warning-output').style.display = "none";
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 & this.status == 200) {
+                document.getElementById('search-output').innerHTML = this.responseText;
+                document.getElementById('search-warning-output').style.display = "none";
+            }
+        }
+        xmlhttp.open('GET', '../scripts/get_usernames.php?u=' + value, true);
+        xmlhttp.send();
+    }
+
 }
 
 // Selain keypress ada keydown dan keyup
---
+++ minecraft/local_test_public/record_keys.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+
+<body>
+  <div>
+    <form action="">
+      <input type="text" name="" id="" />
+      <input type="text" name="" id="" onkeyup="keyPressed(this.value)" />
+    </form>
+    <p class="key"></p>
+    <p class="code"></p>
+
+    <div class="inventory-container" id="search-output"></div>
+    <div class="text-container" id="search-warning-output" style="max-width: 320px; display: none;"></div>
+  </div>
+
+  <script src="record_keys.js"></script>
+</body>
+
+</html>
